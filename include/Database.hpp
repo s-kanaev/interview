734e4ae7be63cd554e5e052d61845868a8c8c426
Sergy Kanaev <ksergy.91@gmail.com>
2014-12-06 11:56:48 +0200

    Comment database class

--- include/Database.hpp
+++ include/Database.hpp
@@ -15,14 +15,15 @@
 #include <boost/thread/thread.hpp>
 
 // synchronous database access class
-// this class should be used with async (boost:asio) wrapper
+// this class should be used with async (boost:asio) wrapper (???)
 // singleton class for database requests
 class Database {
 public:
+    // return singleton instance of the database handler
     static Database& getInstance(void);
 
     /*
-     * connect to database
+     * connect to database and create db talker thread
      * return true if success, false otherwise or if already connected
      */
     bool Connect(std::string _host,
@@ -31,11 +32,10 @@ public:
                  std::string _password,
                  std::string _db_name,
                  std::string _table);
-    // disconnect from database
-    bool Disconnect();
-    // add request and connection object (for reply) to queue
+    // queue disconnect from database
+    void Disconnect();
     /*
-     * add the request and connection object to working queue
+     * add the request and connection object to queue
      */
     void QueueRequest(DBRequest db_request, async_server::connection_ptr connection);
 
@@ -55,33 +55,42 @@ protected:
     void Request(std::string request_string);
     // check for record by id
     bool CheckRecordByID(int id);
+    // do disconnect from db
+    void DoDisconnect(void);
 
     bool m_connected = false;
     bool m_force_reply = false;
 
     std::string m_table;
 
+    bool m_do_disconnect;
+
+    // database reply object
     DBReply m_dbreply;
+    // database records vector for use with reply object
     std::shared_ptr<std::vector<std::shared_ptr<DBRecord>>> m_dbrecords;
+    // cache object
     Cache<bigserial_t, std::shared_ptr<DBRecord>> m_cache;
 
     // db thread mutex
     std::mutex m_db_thread_mutex;
+    // condition variable for database thread
     std::condition_variable m_db_thread_cv;
     // parallel queues for request and connection_objects
-    boost::mutex m_queue_mutex;
     std::queue<DBRequest> m_request_queue;
     std::queue<async_server::connection_ptr> m_connection_queue;
+    // mutex for request and connection queues
+    boost::mutex m_queue_mutex;
 
-    std::shared_ptr<pqxx::connection> m_connection;      // connection to db
-    pqxx::result m_result;              // result of the transaction
+    // database connection
+    std::shared_ptr<pqxx::connection> m_connection;
+    // result of transaction to database
+    pqxx::result m_result;
 
+    // thread to talk with database
     boost::thread m_db_thread;
 
 private:
-    /*
-     * create db_thread
-     */
     Database();
     Database(Database const&);
     Database& operator=(Database const&);
