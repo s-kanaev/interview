53e1f2da7c5b89f9578591c40ec8db1d62866bdb
Sergy Kanaev <ksergy.91@gmail.com>
2014-12-06 11:55:40 +0200

    Comment DBReply class and common part

--- include/DBReply.hpp
+++ include/DBReply.hpp
@@ -6,19 +6,30 @@
 #include <vector>
 #include <memory>
 
+// database reply class
 class DBReply {
 public:
+    // construct an empty reply
     DBReply();
+    // copy constructor
     DBReply(const DBReply &ref);
+    // destructor
     ~DBReply();
 
+    // set kind of reply (ok, not found, bad request)
     void SetKind(DBReplyKind _kind);
+    // set db records for the reply
     void SetRecords(std::shared_ptr<std::vector<std::shared_ptr<DBRecord>>> _records);
+
+    // retrieve reply kind
     DBReplyKind Kind(void) const;
+    // retrieve reply db records
     std::shared_ptr<std::vector<std::shared_ptr<DBRecord>>> Records(void) const;
 
 protected:
+    // kind of the reply
     DBReplyKind m_kind;
+    // supplementary records for this reply (used with GET request only)
     std::shared_ptr<std::vector<std::shared_ptr<DBRecord>>> m_records;
 };
 
