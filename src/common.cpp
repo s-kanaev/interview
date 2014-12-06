a126242abf2b7d074e4e4b050d519d46b51be731
Sergy Kanaev <ksergy.91@gmail.com>
2014-12-06 11:58:05 +0200

    Changed thread pool to shared ptr

--- src/common.cpp
+++ src/common.cpp
@@ -2,9 +2,10 @@
 
 #include <boost/network/tags.hpp>
 #include <boost/network/utils/thread_pool.hpp>
+#include <boost/shared_ptr.hpp>
 
 #define THREADS_COUNT 10
 
 // create thread pool for THREADS_COUNT threads
-boost::network::utils::thread_pool threadPool(THREADS_COUNT);
-
+boost::shared_ptr<boost::network::utils::thread_pool> threadPool(
+    new boost::network::utils::thread_pool(THREADS_COUNT));
