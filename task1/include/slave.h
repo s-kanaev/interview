0c84b9ece76cae4ce3626e103201882093578aa5
sergei <s-kanaev@users.noreply.github.com>
2023-01-25 21:55:24 +0300

    [T1] Add slave data structure description

---
+++ task1/include/slave.h
@@ -0,0 +1,25 @@
+#ifndef _SLAVE_H_
+# define _SLAVE_H_
+
+# include "io-service.h"
+# include "timer.h"
+
+# define SLAVE_UDP_PORT 12345
+
+struct master;
+
+struct slave;
+typedef struct slave slave_t;
+
+struct slave {
+    io_service_t *iosvc;
+    tmr_t *tmr;
+    int udp_socket;
+    struct master *master;
+};
+
+void slave_init(slave_t *sl, io_service_t *iosvc);
+void slave_deinit(slave_t *sl);
+void slave_run(slave_t *sl);
+
+#endif /* _SLAVE_H_ */
