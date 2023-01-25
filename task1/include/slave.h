df887d76a828f19df03bfdfc6b89faaf345e9fa2
sergei <s-kanaev@users.noreply.github.com>
2023-01-25 21:55:24 +0300

    [T1] Enhance slave's data structure and API

--- task1/include/slave.h
+++ task1/include/slave.h
@@ -3,8 +3,9 @@
 
 # include "io-service.h"
 # include "timer.h"
+# include "master.h"
 
-struct master;
+# include <netinet/in.h>
 
 struct slave;
 typedef struct slave slave_t;
@@ -12,11 +13,18 @@ typedef struct slave slave_t;
 struct slave {
     io_service_t *iosvc;
     tmr_t *tmr;
+
+    struct sockaddr_storage local_addr;
+    struct sockaddr_in bcast_addr;
+
     int udp_socket;
-    struct master *master;
+
+    master_t master;
 };
 
-void slave_init(slave_t *sl, io_service_t *iosvc);
+void slave_init(slave_t *sl, io_service_t *iosvc,
+                const char *local_addr,
+                const char *iface);
 void slave_deinit(slave_t *sl);
 void slave_run(slave_t *sl);
 
