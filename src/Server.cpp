d9f89be9a07839dbd0f7e41f32f1f81d6221408d
sergei <s-kanaev@users.noreply.github.com>
2023-01-25 21:55:28 +0300

    Finalize server handler

--- src/Server.cpp
+++ src/Server.cpp
@@ -5,22 +5,40 @@
 #include <boost/network/protocol/http/server.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/asio/io_service.hpp>
+#include <boost/asio/signal_set.hpp>
 // #include <json_spirit.h>
 #include <json_spirit_writer_template.h>
 #include <json_spirit_reader_template.h>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
 #include <cstring>
 #include <cstdio>
-#include <csignal>
 
 namespace http = boost::network::http;
 namespace utils = boost::network::utils;
 
-std::shared_ptr<async_server> _server;
-
 // asynchronous server request handler
 struct AsyncRequestHandler {
+protected:
+    // for use with condition variable of post request
+    std::mutex m_mutex;
+    typedef boost::iterator_range<char const *> rValue;
+    void ConnectionReadCallback(rValue input_range,
+                                boost::system::error_code error,
+                                std::size_t size,
+                                async_server::connection_ptr connection,
+                                std::string *result,
+                                std::condition_variable *cv)
+    {
+        std::unique_lock<std::mutex> locker(m_mutex);
+        result->append(input_range.begin());
+        cv->notify_one();
+    }
+
     void HandlePostRequest(async_server::request const& request,
-                           DBRequest *db_request)
+                           DBRequest *db_request,
+                           async_server::connection_ptr connection)
     {
         db_request->request_type = REQUEST_POST;
         PostRequest *_request = &db_request->any_request.post_request;
@@ -34,25 +52,31 @@ struct AsyncRequestHandler {
         } else {
             bigserial_t id;
             int r = sscanf(request_path.data(), "/users/%llu", &id);
-            if (r > 0) {
-                if (id >= 0) _request->id = id;
-                else {
-                    db_request->request_type = REQUEST_INVALID;
-                    return;
-                }
+            if ((r > 0) && (id >= 0)) {
+                _request->id = id;
             } else {
                 db_request->request_type = REQUEST_INVALID;
                 return;
             }
         }
+        std::condition_variable cv;
+        std::unique_lock<std::mutex> locker(m_mutex);
+        connection->read(boost::bind(&AsyncRequestHandler::ConnectionReadCallback, this, _1, _2, _3, _4, &request_body, &cv));
+        cv.wait(locker);
+        m_mutex.unlock();
         // decode request_body from json
-        request_body = request.body;
+        //request_body = request.body;
+        if (request_body.empty()) {
+            db_request->request_type = REQUEST_INVALID;
+            return;
+        }
         json_spirit::Value mval;
         json_spirit::Object obj;
         bool success;
         success = json_spirit::read_string<std::string, json_spirit::Value>(request_body, mval);
         if (success) {
-            if (mval.type() == json_spirit::obj_type) {
+            if ((mval.type() == json_spirit::obj_type) &&
+                (mval.get_obj().size() == 3)) {
                 obj = mval.get_obj();
                 for (int i = 0; i < obj.size(); ++i) {
                     if (obj[i].name_ == "firstName") {
@@ -61,6 +85,11 @@ struct AsyncRequestHandler {
                         _request->last_name = strdup(obj[i].value_.get_str().data());
                     } else if (obj[i].name_ == "birthDate") {
                         _request->birth_date = strdup(obj[i].value_.get_str().data());
+                    } else {
+                        db_request->request_type = REQUEST_INVALID;
+                        if (_request->first_name) free(_request->first_name);
+                        if (_request->last_name) free(_request->last_name);
+                        if (_request->birth_date) free(_request->birth_date);
                     }
                 }
             } else {
@@ -70,10 +99,11 @@ struct AsyncRequestHandler {
                 _request->id = 0;
             }
         }
-    };
+    }
 
     void HandleDeleteRequest(async_server::request const& request,
-                             DBRequest *db_request)
+                             DBRequest *db_request,
+                             async_server::connection_ptr connection)
     {
         db_request->request_type = REQUEST_DELETE;
         DeleteRequest *_request = &db_request->any_request.delete_request;
@@ -87,10 +117,11 @@ struct AsyncRequestHandler {
         } else {
             db_request->request_type = REQUEST_INVALID;
         }
-    };
+    }
 
     void HandleGetRequest(async_server::request const& request,
-                          DBRequest *db_request)
+                          DBRequest *db_request,
+                          async_server::connection_ptr connection)
     {
         db_request->request_type = REQUEST_GET;
         GetRequest *_request = &db_request->any_request.get_request;
@@ -99,7 +130,7 @@ struct AsyncRequestHandler {
         // retrieve id
         if (request_path == "/users") {
             _request->id = 0;
-        } {
+        } else {
             bigserial_t id;
             int r = sscanf(request_path.data(), "/users/%llu", &id);
             if (r > 0) {
@@ -108,24 +139,26 @@ struct AsyncRequestHandler {
                 db_request->request_type = REQUEST_INVALID;
             }
         }
-    };
+    }
 
+public:
     void operator()(async_server::request const& request,
-                     async_server::connection_ptr connection)
+                    async_server::connection_ptr connection)
     {
         DBRequest db_request;
-        std::string request_path;
         if (request.method == "POST") {
-            HandlePostRequest(request, &db_request);
+            HandlePostRequest(request, &db_request, connection);
         } else if (request.method == "DELETE") {
-            HandleDeleteRequest(request, &db_request);
+            HandleDeleteRequest(request, &db_request, connection);
         } else if (request.method == "GET") {
-            HandleGetRequest(request, &db_request);
-        } else db_request.request_type = REQUEST_INVALID;
+            HandleGetRequest(request, &db_request, connection);
+        } else {
+            db_request.request_type = REQUEST_INVALID;
+        }
         // enqueue request to database
         Database::getInstance().QueueRequest(db_request, connection);
         // return
-    };
+    }
 };
 
 // asynchronous server reply part
@@ -133,13 +166,19 @@ std::string WriteDBRecordAsJSON(DBRecord *db_record)
 {
     json_spirit::Object db_record_obj;
     std::string str;
-    db_record_obj.push_back(json_spirit::Pair("id", std::to_string(db_record->id)));
+    db_record_obj.push_back(json_spirit::Pair("id", (boost::int64_t)db_record->id));
     db_record_obj.push_back(json_spirit::Pair("firstName", db_record->first_name));
     db_record_obj.push_back(json_spirit::Pair("lastName", db_record->last_name));
     db_record_obj.push_back(json_spirit::Pair("birthDate", db_record->birth_date));
     str = json_spirit::write_string<json_spirit::Value>(db_record_obj, json_spirit::pretty_print);
     return str;
 }
+
+static async_server::response_header common_headers[] = {
+    {"Connection", "close"},
+    {"Content-Type", "text/plain"},
+    {"Content-Length", "0"}
+};
 
 void ServerSendReply(DBReply db_reply,
                      async_server::connection_ptr connection)
@@ -150,7 +189,7 @@ void ServerSendReply(DBReply db_reply,
         case REPLY_OK:
             // set reply state
             connection->set_status(async_server::connection::ok);
-            reply_string = "200 OK\n\n";
+            reply_string = "200 OK";
             // if there are any db records available - post them
             std::vector<std::shared_ptr<DBRecord>> *db_records;
             db_records = db_reply.Records().get();
@@ -158,41 +197,49 @@ void ServerSendReply(DBReply db_reply,
                 case 0:
                     break;
                 case 1:
+                    reply_string.append("\n\n");
                     one_record_string = WriteDBRecordAsJSON(db_records->at(0).get());
                     reply_string.append(one_record_string);
                     break;
                 default:
-                    // post '['
-                    reply_string.append("[");
+                    reply_string.append("\n\n");
+                     // post '['
+                    reply_string.append("[\n");
                     // post each of the record
                     for (int i = 0; i < db_records->size(); ++i) {
                         one_record_string = WriteDBRecordAsJSON(db_records->at(i).get());
                         reply_string.append(one_record_string);
+                        reply_string.append(", ");
                     }
                     // post ']'
-                    reply_string.append("]");
+                    reply_string.append("\n]");
                     break;
             }
             break;
         case REPLY_NOT_FOUND:
             // set reply state
             connection->set_status(async_server::connection::not_found);
-            reply_string = "404 Not Found\n\n";
+            reply_string = "404 Not Found";
             break;
         case REPLY_BAD_REQUEST:
             // set reply state
             connection->set_status(async_server::connection::bad_request);
-            reply_string = "400 Bad Request\n\n";
+            reply_string = "400 Bad Request";
             break;
     }
+    common_headers[2].value = boost::lexical_cast<std::string>(reply_string.length());
+    connection->set_headers(boost::make_iterator_range(common_headers, common_headers+2));
     // send the reply
     connection->write(reply_string);
 };
 
-void Signal_INT_TERM_handler(int signum)
+// server shutdown
+void Signal_INT_TERM_handler(const boost::system::error_code& error,
+                             int signal,
+                             boost::shared_ptr<async_server> server_instance)
 {
-    _server->stop();
-    exit(0);
+    if (!error)
+        server_instance->stop();
 }
 
 /*
@@ -207,17 +254,25 @@ void Signal_INT_TERM_handler(int signum)
 void RunServer(std::string address_str, std::string port_str)
 {
     AsyncRequestHandler request_handler;
-    // cppnetlib 0.11.0-final
+    boost::shared_ptr<async_server> _server;
+
     async_server::options options(request_handler);
     options.address(address_str)
            .port(port_str)
-           .thread_pool(threadPool);
-//            .reuse_address(true) // FIXME: ???
-//            .io_service();       // FIXME: ???
-    _server = std::shared_ptr<async_server>(
+           .thread_pool(threadPool)
+           .io_service(iOService);
+ //            .reuse_address(true) // FIXME: ???
+   _server = boost::shared_ptr<async_server>(
         new async_server(async_server::options(options)));
 
     // TODO: set SIGINT and SIGTERM handlers
-    _server->run(); // it will block, heh?
-    //(void)threadPool;
+    boost::asio::signal_set signals(*iOService, SIGINT, SIGTERM);
+    signals.async_wait(boost::bind(Signal_INT_TERM_handler, _1, _2, _server));
+
+    _server->run(); // it will block
+
+    threadGroup->interrupt_all();
+    iOServiceWork.reset();
+    iOService->stop();
+    threadGroup->join_all();
 }
