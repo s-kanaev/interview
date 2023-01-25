310a2fdcc54db0a7ec540877fdaef4eff82b82a5
sergei <s-kanaev@users.noreply.github.com>
2023-01-25 21:55:52 +0300

    [CQG] Implement basic single pair of parenthesis checker

---
+++ cqg/include/ParenthesisChecker.h
@@ -0,0 +1,122 @@
+#ifndef PARENTHESIS_CHECKER_H
+# define PARENTHESIS_CHECKER_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * Parenthis checker for single type of brackets
+ * i.e. a single pair of opening and closing
+ * brackets (e.g. '(' and ')').
+ *
+ * An instance is constructed with no arguments.
+ * An instance must be reset before each and every
+ * line to be processed.
+ *
+ * Processing is done through \c validate call.
+ *
+ * The call to \c validate method returns whether
+ * the block is valid.
+ *
+ * The failure result is cumultive.
+ * I.e. all consequent calls afyer a failure will fail.
+ *
+ * Due to a single pair of brackets in question there
+ * is only a single failure reason: there is closing bracket
+ * while checker is wating for the opening one.
+ *
+ * Typical usage:
+ * \code
+ * bool result = true;
+ * while (input file has another line) {
+ *     checker.reset('a', 'b');
+ *     while (result && read block of line) {
+ *         result = checker.validate(block.start, block.end);
+ *     }
+ *     result = checker.done();
+ * }
+ * \endcode
+ */
+class ParenthesisChecker {
+public:
+    /*** types ***/
+    enum class State {
+        wait_for_open,
+        wait_for_any,
+        finish          = wait_for_open,    /// the only valid state after
+                                            /// a call to \c done
+    };
+
+    struct Context {
+        ssize_t     openIndex   = 0;    /// current open bracket index
+                                        /// less than nil indicates that
+                                        /// there was a superfluous closing bracket
+                                        /// in effect while positive value
+                                        /// indicates that there is this many
+                                        /// open brackets
+        size_t      position    = 0;    /// position in the last block tested
+
+        void newBlock();
+    };
+
+private:
+    /*** data ***/
+    char    _opening;
+    char    _closing;
+    State   _state;
+    Context _context;
+
+   /*** functions ***/
+   bool _validateWaitForOpen(char chr);
+   bool _validateWaitForAny(char chr);
+
+   void _opened();
+   void _closed();
+
+public:
+    /*** API ***/
+    /**
+     * c-tor
+     * Defaults opening bracket to '('
+     * and the closing one to ')'
+     */
+    ParenthesisChecker();
+    ~ParenthesisChecker();
+
+    /**
+     * Reset the checker
+     * \param opening open bracket symbol
+     * \param closing closing bracker symbol
+     * \return \c true if reset is successfull, \c false otherwise
+     *
+     * \c false is returned if \c opening is the same as \c closing
+     *
+     * If reset fails the state of the checker is not changed.
+     */
+    bool reset(char opening, char closing);
+
+    /**
+     * Validate the block
+     * \param str block start
+     * \param length block length
+     * \return \c true if block is still valid
+     */
+    bool validate(const char *str, size_t length);
+
+    /**
+     * Notify the checker that the line is finished.
+     * The result is the same as per \c validate call
+     * except that current state is validated
+     */
+    bool done();
+
+    const Context& getContext() const {
+        return _context;
+    }
+
+    State getState() const {
+        return _state;
+    }
+};
+
+#endif /* PARENTHESIS_CHECKER_H */
---
+++ cqg/lib/ParenthesisChecker.cpp
@@ -0,0 +1,97 @@
+#include "ParenthesisChecker.h"
+
+#include <assert.h>
+
+void ParenthesisChecker::Context::newBlock() {
+    position = 0;
+}
+
+ParenthesisChecker::ParenthesisChecker()
+: _opening('('),
+  _closing(')')
+{
+    /* empty */
+}
+
+ParenthesisChecker::~ParenthesisChecker() {
+    /* empty */
+}
+
+bool ParenthesisChecker::reset(char opening, char closing) {
+    if (opening == closing) {
+        return false;
+    }
+
+    _opening = opening;
+    _closing = closing;
+
+    _context = Context();
+}
+
+bool ParenthesisChecker::validate(const char *str, size_t length) {
+    const char *current = str;
+    const char *last = str + length;
+    bool result = true;
+
+    for (; result && current != last; ++current, ++_context.position) {
+        switch (_state) {
+            case State::wait_for_open :
+                result = _validateWaitForOpen(*current);
+                break;
+
+            case State::wait_for_any :
+                result = _validateWaitForAny(*current);
+                break;
+
+            default:
+                assert(0);
+                break;
+        }
+
+        if (!result) {
+            break;
+        }
+    }
+
+    return result;
+}
+
+bool ParenthesisChecker::done() {
+    return !_context.openIndex;
+}
+
+void ParenthesisChecker::_opened() {
+    ++ _context.openIndex;
+}
+
+void ParenthesisChecker::_closed() {
+    -- _context.openIndex;
+}
+
+bool ParenthesisChecker::_validateWaitForOpen(char chr) {
+    if (chr == _opening) {
+        _opened();
+
+        _state = State::wait_for_any;
+    }
+    else if (chr == _closing) {
+        return false;
+    }
+
+    return true;
+}
+
+bool ParenthesisChecker::_validateWaitForAny(char chr) {
+    if (chr == _opening) {
+        _opened();
+    }
+    else if (chr == _closing) {
+        _closed();
+
+        if (!_context.openIndex) {
+            _state = State::wait_for_open;
+        }
+    }
+
+    return true;
+}
