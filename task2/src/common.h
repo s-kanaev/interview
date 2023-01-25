89ac50dc03ff622da6a210b8f58b9f353a40274d
sergei <s-kanaev@users.noreply.github.com>
2023-01-25 21:55:26 +0300

    [T2] Shift naming constants from driver core to common source header file

--- task2/src/common.h
+++ task2/src/common.h
@@ -3,6 +3,20 @@
 
 # include <stddef.h>
 
+# define DIR         "/tmp/driver"
+# define DIR_LEN     (sizeof(DIR) - 1)
+
+# define MAX_DIGITS  (10)
+
+# define SUFFIX      "drv"
+# define SUFFIX_LEN  (sizeof(SUFFIX) - 1)
+
+# define DOT         "."
+# define DOT_LEN     (sizeof(DOT) - 1)
+
+# define SLASH       "/"
+# define SLASH_LEN   (sizeof(SLASH) - 1)
+
 int allocate_unix_socket(const char *name, size_t name_len);
 
 #endif
