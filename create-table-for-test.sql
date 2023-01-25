37eaa413d6beb881b61b8fa856f21692dbf40417
sergei <s-kanaev@users.noreply.github.com>
2023-01-25 21:55:28 +0300

    Add test-table creation and deletion sql scripts

---
+++ create-table-for-test.sql
@@ -0,0 +1,106 @@
+create table test_table(
+    id bigserial PRIMARY KEY,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    birth_date varchar(50) NOT NULL
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One1', 'Two1', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One2', 'Two2', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One3', 'Two3', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One4', 'Two4', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One5', 'Two5', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One6', 'Two6', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One7', 'Two7', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One8', 'Two8', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One9', 'Two9', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One10', 'Two10', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One11', 'Two11', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One12', 'Two12', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One13', 'Two13', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One14', 'Two14', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One15', 'Two15', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One16', 'Two16', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One17', 'Two17', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One18', 'Two18', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One19', 'Two19', '01-01-1990'
+);
+insert into test_table (
+    first_name, last_name, birth_date)
+    values (
+    'One20', 'Two20', '01-01-1990'
+);
