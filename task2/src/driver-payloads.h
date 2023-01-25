5609a0415f45a327a38df32eea274c55c0481436
sergei <s-kanaev@users.noreply.github.com>
2023-01-25 21:55:26 +0300

    [T2] Introduce two driver payloads sets

---
+++ task2/src/driver-payloads.h
@@ -0,0 +1,10 @@
+#ifndef _DRIVER_PAYLOADS_H_
+# define _DRIVER_PAYLOADS_H_
+
+# include "driver-core.h"
+# include "containers.h"
+
+void driver_payload_one(driver_payload_t *dp, unsigned int slot);
+void driver_payload_two(driver_payload_t *dp, unsigned int slot);
+
+#endif /* _DRIVER_PAYLOADS_H_ */
